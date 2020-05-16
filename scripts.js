84fba498a13edaebe6213b397d3654fb4d2f1b43
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-05-16 18:05:04 -0500

    appended total monthly cost to DOM

--- scripts.js
+++ scripts.js
@@ -3,11 +3,12 @@ console.log('in scripts.js');
 $(document).ready(onReady);
 
 let employees = [];
+let displayMonthlyCost = 0;
+let intTotalMonthlyCost = 0;
 
 function onReady() {
     console.log('in jquery');
-    $('#submitButton').on('click', handleSubmit)
-    $('#submitButton').on('click', monthlyCost)
+    $('#submitButton').on('click', handleSubmit);
 }
 
 function handleSubmit(){
@@ -21,11 +22,46 @@ function handleSubmit(){
         annualSalary: $('#annualSalary').val(),
     };
     employees.push(newEmpObject);
-    console.log(newEmpObject);
-    console.log(employees);
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNumber').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+    monthlyCost(employees);
 }
 
-function monthlyCost(){
+function monthlyCost(array){
     console.log('in monthlyCost');
-    
+    let monthlyCostArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const employeeSalary = array[i].annualSalary;
+        let monthlyCost = employeeSalary / 12;
+        monthlyCostArray.push(monthlyCost);
+    }
+    console.log(monthlyCostArray);
+    totalMonthlyCost(monthlyCostArray)
+}
+
+function totalMonthlyCost(array) {
+    console.log('in totalMonthlyCost');
+    let intTotalMonthlyCost = 0;
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        intTotalMonthlyCost += element;
+    }
+    console.log(intTotalMonthlyCost);
+    $('#monthlyCosts').empty();
+    $('#monthlyCosts').append(intTotalMonthlyCost);
+    if (intTotalMonthlyCost > 20000) {
+        $('#costBackground').addClass('red');
+    }
+    else{
+
+    }
+}
+
+function appendToDom(value){
+    $('#monthlyCosts').empty();
+    $('#monthlyCosts').append(value)
 }
